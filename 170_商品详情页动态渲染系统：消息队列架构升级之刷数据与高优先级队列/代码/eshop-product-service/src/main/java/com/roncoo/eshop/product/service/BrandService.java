b5236b23014e28194af8a1c1620a1e1f900fc830
chen <chen@qq.com>
2019-09-05 00:39:05 +0800

    1

---
+++ 170_商品详情页动态渲染系统：消息队列架构升级之刷数据与高优先级队列/代码/eshop-product-service/src/main/java/com/roncoo/eshop/product/service/BrandService.java
@@ -0,0 +1,15 @@
+package com.roncoo.eshop.product.service;
+
+import com.roncoo.eshop.product.model.Brand;
+
+public interface BrandService {
+	
+	public void add(Brand brand, String operationType);
+	
+	public void update(Brand brand, String operationType);
+	
+	public void delete(Long id, String operationType);
+	
+	public Brand findById(Long id);
+	
+}
